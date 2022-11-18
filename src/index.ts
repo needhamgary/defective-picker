68e51537411660db9cd16e15e31376a3803cff03
loveisglitchy <uncleneedham@gmail.com>
2022-11-18 23:54:28 +0000

    start

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import { Client, GatewayIntentBits } from "discord.js";
+import { Sern } from "@sern/handler";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent, //Make sure this is enabled for text commands!
+  ],
+});
+//View docs for all options
+Sern.init({
+  client,
+  defaultPrefix: "!", // removing defaultPrefix will shut down text commands
+  commands: "dist/commands",
+  events: "dist/events",
+});
+
+client.login(process.env.DISCORD_TOKEN);
